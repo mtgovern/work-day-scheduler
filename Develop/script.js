de1be8111953b1111e677ce4578a63e5e3031bc9
Maria McGovern <mtgovern@me.com>
2020-03-11 22:17:05 -0400

    add saving to local storage

--- Develop/script.js
+++ Develop/script.js
@@ -41,3 +41,53 @@ function colorChange () {
     });
 }
 
+// text for event is saved in local storage
+$(".saveBtn").click(function() {
+    event = $(this).siblings(".input").val();
+    console.log(event);
+    hour = $(this).siblings(".hour").text();
+    console.log(hour);
+    localStorage.setItem(hour, JSON.stringify(event));
+    
+    colorChange ();
+    renderText ();
+})
+
+function renderText () {
+    var event9 = JSON.parse(localStorage.getItem("9:00 am"));
+    $("#9").val("");
+    $("#9").val(event9);
+    
+    var event10 = JSON.parse(localStorage.getItem("10:00 am"));
+    $("#10").val("");
+    $("#10").val(event10);
+    
+    var event11 = JSON.parse(localStorage.getItem("11:00 am"));
+    $("#11").val("");
+    $("#11").val(event11);
+    
+    var event12 = JSON.parse(localStorage.getItem("12:00 pm"));
+    $("#12").val("");
+    $("#12").val(event12);
+    
+    var event1 = JSON.parse(localStorage.getItem("1:00 pm"));
+    $("#13").val("");
+    $("#13").val(event1);
+
+    var event2 = JSON.parse(localStorage.getItem("2:00 pm"));
+    $("#14").val("");
+    $("#14").val(event2);
+
+    var event3 = JSON.parse(localStorage.getItem("3:00 pm"));
+    $("#15").val("");
+    $("#15").val(event3);
+
+    var event4 = JSON.parse(localStorage.getItem("4:00 pm"));
+    $("#16").val("");
+    $("#16").val(event4);
+
+    var event5 = JSON.parse(localStorage.getItem("5:00 pm"));
+    $("#17").val("");
+    $("#17").val(event5);
+}
+
